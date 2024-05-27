d238035f82f7c24f99e19c83158bb130a409a663
Поляков Дмитрий Игоревич <dpolyakov@mmtr.ru>
2024-05-27 16:44:09 +0300

    Создание homepage, страницы с добавлением тако и страницы с заказом.

---
+++ src/main/java/tacos/controller/OrderController.java
@@ -0,0 +1,27 @@
+package tacos.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import tacos.model.TacoOrder;
+
+@Slf4j
+@Controller
+@SessionAttributes("tacoOrder")
+@RequestMapping("/orders")
+public class OrderController {
+  @GetMapping("/current")
+  public String orderForm(){
+    return "orderForm";
+  }
+  @PostMapping()
+  public String processOrder(TacoOrder order, SessionStatus sessionStatus){
+      log.info("Order submitted: {}", order);
+      sessionStatus.setComplete();
+      return "redirect:/";
+  }
+}
